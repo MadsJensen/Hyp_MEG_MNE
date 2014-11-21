c1659f8ca66ce9e6e664df3b1a8808ab8e1e2f9a
mads jensen <mje.mads@gmail.com>
2014-11-21 11:01:54 +0100

    updated

--- extract_ts.py
+++ extract_ts.py
@@ -1,28 +1,27 @@
 
 import numpy as np
 import numpy.random as npr
-import matplotlib.pyplot as plt
 import mne
 import os
 import socket
 import networkx as nx
 
 from mne.minimum_norm import (apply_inverse_epochs, read_inverse_operator)
+from mne.baseline import rescale
 from nitime import TimeSeries
-from nitime.analysis import MTCoherenceAnalyzer
-from nitime.viz import drawmatrix_channels
+from nitime.analysis import CoherenceAnalyzer
 
 # Setup paths and prepare raw data
 hostname = socket.gethostname()
 
 if hostname == "wintermute":
     data_path = "/home/mje/mnt/Hyp_meg/scratch/Tone_task_MNE/"
+    subjects_dir = "/home/mje/mnt/Hyp_meg/scratch/fs_subjects_dir/"
 else:
     data_path = "/scratch1/MINDLAB2013_18-MEG-HypnosisAnarchicHand/" + \
                 "Tone_task_MNE/"
     subjects_dir = "/scratch1/MINDLAB2013_18-MEG-HypnosisAnarchicHand/" + \
-                    "fs_subjects_dir"
-
+                   "fs_subjects_dir"
 
 
 epochs_fnormal = data_path + "tone_task_normal-epo.fif"
@@ -53,27 +52,28 @@ epochs_hyp = mne.read_epochs(epochs_fhyp)
 epochs_normal = epochs_normal["press"]
 epochs_hyp = epochs_hyp["press"]
 
-epochs_normal.crop(tmin=0, tmax=0.5)
-epochs_hyp.crop(tmin=0, tmax=0.5)
+# epochs_normal.crop(tmin=0, tmax=0.5)
+# epochs_hyp.crop(tmin=0, tmax=0.5)
 
 
 # %%
 stcsNormal = apply_inverse_epochs(epochs_normal, inverse_normal, lambda2,
-                                method, pick_ori="normal",
-                                return_generator=True)
+                                  method, pick_ori="normal",
+                                  return_generator=True)
 stcsHyp = apply_inverse_epochs(epochs_hyp, inverse_hyp, lambda2,
-                                method, pick_ori="normal",
-                                return_generator=True)
+                               method, pick_ori="normal",
+                               return_generator=True)
 
 # Get labels for FreeSurfer 'aparc' cortical parcellation with 34 labels/hemi
-labelsTest = mne.read_labels_from_annot('subject_1', parc='PALS_B12_Brodmann',
+labels = mne.read_labels_from_annot('subject_1', parc='PALS_B12_Brodmann',
                                     regexp="Brodmann",
                                     subjects_dir=subjects_dir)
 
 # Average the source estimates within each label using sign-flips to reduce
 # signal cancellations, also here we return a generator
 src_normal = inverse_normal['src']
-labelTsNormal = mne.extract_label_time_course(stcsNormal, labels, src_normal,
+labelTsNormal = mne.extract_label_time_course(stcsNormal, labels,
+                                              src_normal,
                                               mode='mean_flip',
                                               return_generator=False)
 src_hyp = inverse_hyp['src']
@@ -81,17 +81,30 @@ labelTsHyp = mne.extract_label_time_course(stcsHyp, labels, src_hyp,
                                            mode='mean_flip',
                                            return_generator=False)
 
+# standardize TS's
+labelTsNormalZscore = []
+for j in range(len(labelTsNormal)):
+    labelTsNormalZscore += [rescale(labelTsNormal[j], epochs_normal.times,
+                                    baseline=(None, -0.7), mode="zscore")]
+
+labelTsHypZscore = []
+for j in range(len(labelTsHyp)):
+    labelTsHypZscore += [rescale(labelTsHyp[j], epochs_hyp.times,
+                                 baseline=(None, -0.7), mode="zscore")]
+
+# crop zscored TS
+fromTime = np.argmax(epochs_normal.times == 0)
+toTime = np.argmax(epochs_normal.times == 0.5)
+
+labelTsNormalZscoreCrop = []
+for j in range(len(labelTsNormalZscore)):
+    labelTsNormalZscoreCrop += [labelTsNormalZscore[j][:, fromTime:toTime]]
+
+labelTsHypZscoreCrop = []
+for j in range(len(labelTsHypZscore)):
+    labelTsHypZscoreCrop += [labelTsHypZscore[j][:, fromTime:toTime]]
+
 # %%
-f_lw, f_up = 13, 20  # lower & upper limit for frequencies
-
-cohMatrixNormal = np.empty([np.shape(labelTsNormal)[1],
-                            np.shape(labelTsNormal)[1],
-                            np.shape(labelTsNormal)[0]])
-cohMatrixHyp = np.empty([np.shape(labelTsHyp)[1],
-                            np.shape(labelTsHyp)[1],
-                            np.shape(labelTsHyp)[0]])
-
-
 labels_name = []
 for label in labels:
     labels_name += [label.name]
@@ -99,38 +112,50 @@ for label in labels:
 cohListNormal = []
 cohListHyp = []
 
-for j in range(cohMatrixNormal.shape[2]):
-    nits = TimeSeries(labelTsNormal[j], 
+for j in range(len(labelTsNormal)):
+    nits = TimeSeries(labelTsNormalZscore[j],
                       sampling_rate=epochs_normal.info["sfreq"])
     nits.metadata["roi"] = labels_name
 
-    cohListNormal += [MTCoherenceAnalyzer(nits)]
-    
+    cohListNormal += [CoherenceAnalyzer(nits)]
 
-for j in range(cohMatrixHyp.shape[2]):
-    nits = TimeSeries(labelTsHyp[j],
+
+for j in range(len(labelTsHyp)):
+    nits = TimeSeries(labelTsHypZscore[j],
                       sampling_rate=epochs_hyp.info["sfreq"])
     nits.metadata["roi"] = labels_name
 
-    cohListHyp += [MTCoherenceAnalyzer(nits)]
+    cohListHyp += [CoherenceAnalyzer(nits)]
 
+# %% extract coherence values
+f_lw, f_up = 30, 50  # lower & upper limit for frequencies
 
-    # confine analysis to Aplha (8  12 Hz)
-freq_idx = np.where((cohListHyp[0].frequencies >= f_lw) * \
+cohMatrixNormal = np.empty([len(labels_name),
+                            len(labels_name),
+                            len(labelTsNormalZscore)])
+cohMatrixHyp = np.empty([len(labels_name),
+                         len(labels_name),
+                         len(labelTsHypZscore)])
+
+# confine analysis to Aplha (8  12 Hz)
+freq_idx = np.where((cohListHyp[0].frequencies >= f_lw) *
                     (cohListHyp[0].frequencies <= f_up))[0]
 
-    # compute average coherence &  Averaging on last dimension
+# compute average coherence &  Averaging on last dimension
+for j in range(cohMatrixNormal.shape[2]):
+    cohMatrixNormal[:, :, j] = np.mean(
+        cohListNormal[j].coherence[:, :, freq_idx], -1)
+
 for j in range(cohMatrixHyp.shape[2]):
-    cohMatrixNormal[:, :, j] = np.mean(cohListNormal[j].coherence[:, :, freq_idx], -1)
-    cohMatrixHyp[:, :, j] = np.mean(cohListHyp[j].coherence[:, :, freq_idx], -1)
+    cohMatrixHyp[:, :, j] = np.mean(
+        cohListHyp[j].coherence[:, :, freq_idx], -1)
 
 
 # %%
 fullMatrix = np.concatenate([cohMatrixNormal, cohMatrixHyp], axis=2)
 
-threshold = np.median(fullMatrix[np.nonzero(fullMatrix)]) \
-            + np.std(fullMatrix[np.nonzero(fullMatrix)])
-
+threshold = np.median(fullMatrix[np.nonzero(fullMatrix)]) + \
+    np.std(fullMatrix[np.nonzero(fullMatrix)])
 
 binMatrixNormal = cohMatrixNormal > threshold
 binMatrixHyp = cohMatrixHyp > threshold
@@ -146,13 +171,13 @@ for j in range(binMatrixHyp.shape[2]):
 
 
 # %%
-degreesHyp = []
-for j, trial in enumerate(nxHyp):
-    degreesHyp += [trial.degree()]
-
 degreesNormal = []
 for j, trial in enumerate(nxNormal):
     degreesNormal += [trial.degree()]
+
+degreesHyp = []
+for j, trial in enumerate(nxHyp):
+    degreesHyp += [trial.degree()]
 
 ccNormal = []
 for j, trial in enumerate(nxNormal):
@@ -160,6 +185,7 @@ for j, trial in enumerate(nxNormal):
 ccHyp = []
 for j, trial in enumerate(nxHyp):
     ccHyp += [nx.cluster.clustering(trial)]
+
 
 # %% Permutation test
 def permutation_resampling(case, control, num_samples, statistic):
@@ -183,13 +209,13 @@ def permutation_resampling(case, control, num_samples, statistic):
 
 # %%
 pvalList = []
-for degreeNumber in range(binMatrixNormal.shape[0]):
+for degreeNumber in range(binMatrixHyp.shape[0]):
 
-    postHyp = np.empty(len(ccHyp))
+    postHyp = np.empty(len(degreesHyp))
     for j in range(len(ccHyp)):
         postHyp[j] = ccHyp[j][degreeNumber]
 
-    postNormal = np.empty(len(ccNormal))
+    postNormal = np.empty(len(degreesNormal))
     for j in range(len(postNormal)):
         postNormal[j] = ccNormal[j][degreeNumber]
 
@@ -206,23 +232,23 @@ pvals = np.empty(len(pvalList))
 for j in range(len(pvals)):
     pvals[j] = pvalList[j]["pval"]
 
-rejected, pvals_corrected = mne.stats.bonferroni_correction(pvals)
+rejected, pvals_corrected = mne.stats.fdr_correction(pvals)
 
 corrIndex = pvals_corrected < (0.05)
 
-for i in range(62):
-    if corrIndex[i] == True:
+for i in range(len(labels_name)):
+    if corrIndex[i]:
         print labels_name[i], \
             "pval:", pvalList[i]["pval"], \
             "observed differnce:", pvalList[i]["obsDiff"], \
             "mean random difference:", np.asarray(pvalList[i]["diffs"]).mean()
 
 # %%
-rejected, pvals_corrected = mne.stats.fdr_correction(pvals)
+# rejected, pvals_corrected = mne.stats.fdr_correction(pvals)
 
-corrIndex = pvals_corrected < 0.05
+# corrIndex = pvals_corrected < 0.05
 
-for i in range(62):
-    if corrIndex[i] == True:
-        print labels_name[i], \
-            "pval:", pvals_corrected[i]
+# for i in range(len(labels_name)):
+#     if corrIndex[i]:
+#         print labels_name[i], \
+#             "pval:", pvals_corrected[i]
