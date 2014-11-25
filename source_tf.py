e9ab36dd3b2b20862fd2427d4bc9c2fdcf67f474
mads jensen <mje.mads@gmail.com>
2014-11-25 15:02:48 +0100

    updates and added functions

--- source_tf.py
+++ source_tf.py
@@ -1,7 +1,14 @@
-import mne
-import socket
+import cPickle
 import os
-from mne.minimum_norm import read_inverse_operator, source_band_induced_power
+import socket
+
+import matplotlib.pyplot as plt
+import mne
+import numpy as np
+from mne.minimum_norm import (read_inverse_operator,
+                              source_band_induced_power,
+                              compute_source_psd_epochs)
+
 
 # Setup paths and prepare raw data
 hostname = socket.gethostname()
@@ -15,11 +22,10 @@ else:
                 "Tone_task_MNE/"
     subjects_dir = "/scratch1/MINDLAB2013_18-MEG-HypnosisAnarchicHand/" + \
                    "fs_subjects_dir"
-    n_jobs = 10
+    n_jobs = 8
 
 # change dir to save files the rigth place
 os.chdir(data_path)
-
 
 ###############################################################################
 epochs_fnormal = data_path + "tone_task_normal-epo.fif"
@@ -38,33 +44,150 @@ epochs_hyp = epochs_hyp["press"]
 
 
 # Compute a source estimate per frequency band
-bands = dict(alpha=[8, 12], beta=[13, 20])
+bands = dict(alpha=[8, 12],
+             beta=[13, 20])
 
-stcs_normal = source_band_induced_power(epochs_normal, inverse_normal, bands,
-                                        n_cycles=2, use_fft=False,
-                                        baseline=(-1, -0-7),
-                                        baseline_mode="zscore",
-                                        n_jobs=n_jobs)
-stcs_hyp = source_band_induced_power(epochs_hyp, inverse_hyp, bands,
-                                     n_cycles=2, use_fft=False,
-                                     baseline=(-1, -0-7),
-                                     baseline_mode="zscore",
-                                     n_jobs=n_jobs)
+#             gamma_low=[30, 48],
+#             gamma_high=[52, 90])
+
+snr = 1.0  # Standard assumption for average data but using it for single trial
+lambda2 = 1.0 / snr ** 2
+
+stcs_normal = []
+for j in range(len(epochs_normal)):
+    stcs_normal += [source_band_induced_power(epochs_normal[j], inverse_normal,
+                                              bands,
+                                              lambda2=lambda2,
+                                              method="MNE",
+                                              n_cycles=2,
+                                              use_fft=False,
+                                              baseline=(-0.9, -0.7),
+                                              baseline_mode="zscore",
+                                              n_jobs=n_jobs)]
+
+stcs_hyp = []
+for j in range(len(epochs_hyp)):
+    stcs_hyp += [source_band_induced_power(epochs_hyp[j], inverse_hyp,
+                                           bands,
+                                           lambda2=lambda2,
+                                           method="MNE",
+                                           n_cycles=2, use_fft=False,
+                                           baseline=(-0.9, -0.7),
+                                           baseline_mode="zscore",
+                                           n_jobs=n_jobs)]
+
+
+for j, stc in enumerate(stcs_hyp):
+    stc["alpha"].crop(-0.1, 0.7)
+    stc["beta"].crop(-0.1, 0.7)
+
+cPickle.dump(stcs_normal, open("stcs_normal_souce_induced_A_B.p", "wb"))
+cPickle.dump(stcs_hyp, open("stcs_hyp_souce_induced_A_B.p", "wb"))
 
 # for b, stc in stcs.iteritems():
 #     stc.save('induced_power_%s' % b)
 
+
 ###############################################################################
 # plot mean power
-import matplotlib.pyplot as plt
-plt.plot(stcs_normal['alpha'].times, stcs_normal['alpha'].data.mean(axis=0),
-         label='Alpha', color='b')
-plt.plot(stcs_hyp['alpha'].times, stcs_hyp['alpha'].data.mean(axis=0),
-         label='Alpha', color='r')
+
+for j, stcs in enumerate(stcs_normal):
+    if j == 0:
+        stcs_normal_alpha = stcs_normal[0]["alpha"].data.mean(axis=0)
+    else:
+        stcs_normal_alpha = np.vstack([stcs_normal_alpha,
+                                       stcs_normal[j]["alpha"].data
+                                       .mean(axis=0)])
+
+for j, stcs in enumerate(stcs_hyp):
+    if j == 0:
+        stcs_hyp_alpha = stcs_hyp[0]["alpha"].data.mean(axis=0)
+    else:
+        stcs_hyp_alpha = np.vstack([stcs_hyp_alpha,
+                                    stcs_hyp[j]["alpha"].data.mean(axis=0)])
+
+
+times = stcs_normal[0]["alpha"].times
+mean_normal = stcs_normal_alpha.mean(axis=0)
+mean_hyp = stcs_hyp_alpha.mean(axis=0)
+std_normal = stcs_normal_alpha.std(axis=0)
+std_hyp = stcs_hyp_alpha.std(axis=0)
+hyp_limits_normal = (mean_normal - std_normal, mean_normal + std_normal)
+plt.fill_between(times, hyp_limits_normal[0], y2=hyp_limits_normal[1],
+                 color='b', alpha=0.5)
+plt.plot(times, mean_normal, label='Normal: Alpha', color='b')
+hyp_limits_hyp = (mean_hyp - std_hyp, mean_hyp + std_hyp)
+plt.fill_between(times, hyp_limits_hyp[0], y2=hyp_limits_hyp[1],
+                 color='b', alpha=0.5)
+plt.plot(times, mean_hyp, label='Hyp: Alpha', color='r')
+plt.plot(times, mean_normal + std_normal, color='b--')
 
 plt.plot(stcs['beta'].times, stcs['beta'].data.mean(axis=0), label='Beta')
 plt.xlabel('Time (ms)')
 plt.ylabel('Power')
 plt.legend()
 plt.title('Mean source induced power')
+plt.show()
+
+
+# %%
+# Compute Power Spectral Density of inverse solution from single epochs
+# define frequencies of interest
+fmin, fmax = 0., 70.
+bandwidth = 4.  # bandwidth of the windows in Hz
+snr = 1.0  # use smaller SNR for raw data
+lambda2 = 1.0 / snr ** 2
+method = "MNE"  # use dSPM method (could also be MNE or sLORETA)
+# compute source space psd in label
+
+# Note: By using "return_generator=True" stcs will be a generator object
+# instead of a list. This allows us so to iterate without having to
+# keep everything in memory.
+
+stcs_normal = compute_source_psd_epochs(epochs_normal, inverse_normal,
+                                        lambda2=lambda2,
+                                        method=method, fmin=fmin, fmax=fmax,
+                                        bandwidth=bandwidth,
+                                        return_generator=True)
+
+# compute average PSD over the first 10 epochs
+# n_epochs = 4
+for i, stc in enumerate(stcs_normal):
+    # if i >= n_epochs:
+    #     break
+    if i == 0:
+        psd_normal = np.mean(stc.data, axis=0)
+    else:
+        psd_normal = np.vstack([psd_normal, np.mean(stc.data, axis=0)])
+
+psd_avg_normal = np.mean(psd_normal, axis=0)
+psd_std_normal = np.std(psd_normal, axis=0)
+freqs = stc.times  # the frequencies are stored here
+
+stcs_hyp = compute_source_psd_epochs(epochs_hyp, inverse_hyp,
+                                     lambda2=lambda2,
+                                     method=method, fmin=fmin, fmax=fmax,
+                                     bandwidth=bandwidth,
+                                     return_generator=True)
+
+# compute average PSD over the first 10 epochs
+for i, stc in enumerate(stcs_hyp):
+    # if i >= n_epochs:
+    #     break
+    if i == 0:
+        psd_hyp = np.mean(stc.data, axis=0)
+    else:
+        psd_hyp = np.vstack([psd_hyp, np.mean(stc.data, axis=0)])
+
+psd_avg_hyp = np.mean(psd_hyp, axis=0)
+psd_std_hyp = np.std(psd_hyp, axis=0)
+freqs = stc.times  # the frequencies are stored here
+
+
+plt.figure()
+plt.plot(freqs, psd_avg_normal, color="b", label="Normal")
+plt.plot(freqs, psd_avg_hyp, color="r", label="Hyp")
+plt.xlabel('Freq (Hz)')
+plt.ylabel('Power Spectral Density')
+plt.legend()
 plt.show()
--- tf_analysis.py
+++ tf_analysis.py
@@ -32,38 +32,41 @@ epochs_hyp = mne.read_epochs(epochs_fhyp)
 epochs_normal = epochs_normal["press"]
 epochs_hyp = epochs_hyp["press"]
 
-
 ###############################################################################
 # Calculate power and intertrial coherence
 
 freqs = np.arange(6, 90, 2)  # define frequencies of interest
-n_cycles = freqs / 2.  # different number of cycle per frequency
-power_normal, itc_normal = tfr_morlet(epochs_normal, freqs=freqs,
-                                      n_cycles=n_cycles, use_fft=False,
-                                      return_itc=True, decim=3, n_jobs=n_jobs)
+n_cycles = freqs / 3.  # different number of cycle per frequency
 
-power_hyp, itc_hyp = tfr_morlet(epochs_hyp, freqs=freqs,
+power_normal = []
+for j in range(len(epochs_normal)):
+    power_normal += [tfr_morlet(epochs_normal[j], freqs=freqs,
                                 n_cycles=n_cycles, use_fft=False,
-                                return_itc=True, decim=3, n_jobs=n_jobs)
+                                return_itc=False, decim=3, n_jobs=1)]
+power_hyp = []
+for j in range(len(epochs_hyp)):
+    power_hyp += [tfr_morlet(epochs_hyp, freqs=freqs,
+                             n_cycles=n_cycles, use_fft=False,
+                             return_itc=False, decim=3, n_jobs=1)]
 
 # Baseline correction can be applied to power or done in plots
 # To illustrate the baseline correction in plots the next line is commented
 # power.apply_baseline(baseline=(-0.5, 0), mode='logratio')
 
 # Inspect power
-power_normal.plot_topo(baseline=(-1, -0.7), mode='zscore',
+power_normal.plot_topo(baseline=(-0.9, -0.7), mode='zscore',
                        title='Average power')
-power_normal.plot([82], baseline=(-1, -0.7), mode='zscore')
+power_normal.plot([82], baseline=(-0.9, -0.7), mode='zscore')
 
 import matplotlib.pyplot as plt
 fig, axis = plt.subplots(1, 2, figsize=(7, 4))
-power.plot_topomap(ch_type='grad', tmin=0.5, tmax=1.5, fmin=8, fmax=12,
-                   baseline=(-0.5, 0), mode='logratio', axes=axis[0],
-                   title='Alpha', vmin=-0.45, vmax=0.45)
-power.plot_topomap(ch_type='grad', tmin=0.5, tmax=1.5, fmin=13, fmax=25,
-                   baseline=(-0.5, 0), mode='logratio', axes=axis[1],
-                   title='Beta', vmin=-0.45, vmax=0.45)
+power_normal[0].plot_topomap(ch_type='grad', tmin=0, tmax=1, fmin=52, fmax=90,
+                             baseline=(-0.9, -0.7), mode='zscore',
+                             axes=axis[0],
+                             title='Beta')
+power_normal[0].plot_topomap(ch_type='grad', tmin=0, tmax=1, fmin=52, fmax=80,
+                             baseline=(-0.9, -0.7), mode='zscore',
+                             vmin=0, vmax=9,
+                             title='Alpha')
 mne.viz.tight_layout()
 
-# Inspect ITC
-itc.plot_topo(title='Inter-Trial coherence', vmin=0., vmax=1., cmap='Reds')
