68513eb447c831a2048dcdbc0896604f25a863ea
mads jensen <mje.mads@gmail.com>
2015-07-03 14:39:40 +0200

    updated for source tfr

--- wrapper_bash.sh
+++ wrapper_bash.sh
@@ -1,5 +1,5 @@
 
-ipython sk_ROI_analysis_PRESS_pre.py
-ipython sk_ROI_analysis_PRESS_post.py
-ipython sk_ROI_analysis_TONE_post.py
-ipython sk_ROI_analysis_TONE_pre.py
+ipython sk_source_tfr_PRESS_pre.py
+ipython sk_source_tfr_TONE_post.py
+ipython sk_source_tfr_PRESS_post.py
+ipython sk_source_tfr_TONE_pre.py
