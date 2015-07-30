3f682d57e5b61c637993cdd5fe5bb5934ffffbab
mads jensen <mje.mads@gmail.com>
2015-07-30 08:04:09 +0200

    Update with loop.
    added loop over labels.

--- sk_source_power_PRESS_pre.py
+++ sk_source_power_PRESS_pre.py
@@ -3,6 +3,7 @@ Created on Wed June 20 2015
 
 @author: mje
 """
+
 import os
 import socket
 import numpy as np
@@ -67,102 +68,110 @@ label_single = [labels[3]]
 snr = 1.0  # Standard assumption for average data but using it for single trial
 lambda2 = 1.0 / snr ** 2
 method = "MNE"
-bands = dict(alpha=[9, 11], beta=[18, 22], gamme_low=[30, 48],
+bands = dict(alpha=[8, 13], beta=[13, 30], gamma_low=[30, 48],
              gamma_high=[52, 88])
 
 band = {bands.keys()[0]: bands.values()[0]}
 
-stcs_normal = []
-for j in range(len(epochs_normal)):
-    print "\n*********************"
-    print "working on %d of %d" % (j+1, len(epochs_normal))
-    print "\n*********************"
+for h in range(len(bands)):
+    band = {bands.keys()[h]: bands.values()[h]}
 
-    stcs_normal += [source_band_induced_power(epochs_normal[j],
-                                              inverse_normal,
-                                              band,
-                                              n_cycles=2,
-                                              use_fft=False,
-                                              baseline=(None, -0.7),
-                                              baseline_mode="zscore",
-                                              n_jobs=n_jobs)]
+    stcs_normal = []
+    for j in range(len(epochs_normal)):
+        print "\n*********************"
+        print "working on %d of %d" % (j+1, len(epochs_normal))
+        print "*********************\n"
 
-stcs_hyp = []
-for j in range(len(epochs_hyp)):
-    print "\n*********************"
-    print "working on %d of %d" % (j+1, len(epochs_hyp))
-    print "\n*********************"
+        stcs_normal += [source_band_induced_power(epochs_normal[j],
+                                                  inverse_normal,
+                                                  band,
+                                                  n_cycles=2,
+                                                  lambda2=lambda2,
+                                                  method=method,
+                                                  use_fft=False,
+                                                  baseline=(None, -0.7),
+                                                  baseline_mode="zscore",
+                                                  n_jobs=n_jobs)]
 
-    stcs_hyp += [source_band_induced_power(epochs_hyp[j],
-                                           inverse_hyp,
-                                           band,
-                                           n_cycles=2,
-                                           use_fft=False,
-                                           baseline=(None, -0.7),
-                                           baseline_mode="zscore",
-                                           n_jobs=n_jobs)]
+    stcs_hyp = []
+    for j in range(len(epochs_hyp)):
+        print "\n*********************"
+        print "working on %d of %d" % (j+1, len(epochs_hyp))
+        print "*********************\n"
 
+        stcs_hyp += [source_band_induced_power(epochs_hyp[j],
+                                               inverse_hyp,
+                                               band,
+                                               n_cycles=2,
+                                               lambda2=lambda2,
+                                               method=method,
+                                               use_fft=False,
+                                               baseline=(None, -0.7),
+                                               baseline_mode="zscore",
+                                               n_jobs=n_jobs)]
 
-[stc["alpha"].crop(-0.2, 0) for stc in stcs_normal]
-[stc["alpha"].crop(-0.2, 0) for stc in stcs_hyp]
+    [stc[band.keys()[0]].crop(-0.2, 0) for stc in stcs_normal]
+    [stc[band.keys()[0]].crop(-0.2, 0) for stc in stcs_hyp]
 
-# Classification setting
-n_splits = 10
-LR = LogisticRegression()
-gnb = GaussianNB()
+    # Classification setting
+    n_splits = 10
+    LR = LogisticRegression()
+    gnb = GaussianNB()
 
-clf = gnb
-clf_names = ["GNB"]
+    clf = gnb
+    clf_names = ["GNB"]
 
-p_results = {}
-score_results = {}
+    p_results = {}
+    score_results = {}
 
-stcs_normal = [stc["alpha"] for stc in stcs_normal]
-stcs_hyp = [stc["alpha"] for stc in stcs_hyp]
+    stcs_normal = [stc[band.keys()[0]] for stc in stcs_normal]
+    stcs_hyp = [stc[band.keys()[0]] for stc in stcs_hyp]
 
-for label in labels:
-    labelTsNormal = mne.extract_label_time_course(stcs_normal,
-                                                  labels=label,
-                                                  src=src_normal,
-                                                  mode='mean_flip',
-                                                  return_generator=False)
+    for label in labels:
+        labelTsNormal = mne.extract_label_time_course(stcs_normal,
+                                                      labels=label,
+                                                      src=src_normal,
+                                                      mode='mean_flip',
+                                                      return_generator=False)
 
-    labelTsHyp = mne.extract_label_time_course(stcs_hyp,
-                                               labels=label,
-                                               src=src_hyp,
-                                               mode='mean_flip',
-                                               return_generator=False)
+        labelTsHyp = mne.extract_label_time_course(stcs_hyp,
+                                                   labels=label,
+                                                   src=src_hyp,
+                                                   mode='mean_flip',
+                                                   return_generator=False)
 
-    X = np.vstack([labelTsNormal, labelTsHyp])
-    X = X[:, 0, :]
-    y = np.concatenate([np.zeros(len(labelTsNormal)),
-                        np.ones(len(labelTsHyp))])
+        X = np.vstack([labelTsNormal, labelTsHyp])
+        X = X[:, 0, :]
+        y = np.concatenate([np.zeros(len(labelTsNormal)),
+                            np.ones(len(labelTsHyp))])
 
-    # X = X * 1e11
-    X_pre = preprocessing.scale(X)
-    cv = StratifiedShuffleSplit(y, n_splits)
-    print "Working on: ", label.name
+        # X = X * 1e11
+        X_pre = preprocessing.scale(X)
+        cv = StratifiedShuffleSplit(y, n_splits)
+        print "Working on: %s in band: %s" % (label.name, band.keys()[0])
 
-    score, permutation_scores, pvalue =\
-        permutation_test_score(
-            clf, X_pre, y, scoring="accuracy",
-            cv=cv, n_permutations=5000,
-            n_jobs=n_jobs)
+        score, permutation_scores, pvalue =\
+            permutation_test_score(
+                clf, X_pre, y, scoring="accuracy",
+                cv=cv, n_permutations=5000,
+                n_jobs=n_jobs)
 
-    score_results[label.name] = score
-    p_results[label.name] = pvalue
+        score_results[label.name] = score
+        p_results[label.name] = pvalue
 
-    outfile_p_name = "p_results_DA_press_power_" +\
-        "dSPM_-02-0_%s_nostd_mean_flip.csv" % clf_names[0]
-    outfile_score_name = "score_results_DA_press_power_" +\
-        "dSPM_-02-0_%s_nostd_mean_flip.csv" % clf_names[0]
+        outfile_p_name = "p_results_DA_press_power" +\
+            "_%s_MNE_-02-0_%s_std_mean_flip.csv" % (band.keys()[0],
+                                                    clf_names[0])
+        outfile_score_name = "score_results_DA_press_power" +\
+            "_%s_MNE_-02-0_%s_std_mean_flip.csv" % (band.keys()[0],
+                                                    clf_names[0])
 
-    with open(outfile_p_name, "w") as outfile:
-        writer = csv.writer(outfile)
-        for key, val in p_results.items():
-            writer.writerow([key, val])
+        with open(outfile_p_name, "w") as outfile:
+            writer = csv.writer(outfile)
+            for key, val in p_results.items():
+                writer.writerow([key, val])
 
-    with open(outfile_score_name, "w") as outfile:
-        writer = csv.writer(outfile)
-        for key, val in score_results.items():
-            writer.writerow([key, val])
+        with open(outfile_score_name, "w") as outfile:
+            writer = csv.writer(outfile)
+            for key, val in score_results.items():
+                writer.writerow([key, val])
